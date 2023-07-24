29b8905688f8b1b03445d60065251828163bc988
Nimasha Dulangi <127924719+NimashaDulangi@users.noreply.github.com>
2023-07-24 15:14:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,42 @@
-Let section = document.querySelectorAll('section');
-Let navLinks = document.querySelectorAll('header nav a');
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
+let section = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
 
 window.onscroll = () => {
     section.forEach(sec => {
-        Let top = window.scrollY;
-        Let offset = sec.offsetTop - 150;
-        Let height = sec.offsetHeight;
-        Let id = sec.getAttribute('id');
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
 
-        if(top >= offset && top < offset + height){
+        if (top >= offset && top < offset + height) {
             navLinks.forEach(links => {
-                links.slassList.remove('active');
-                document.querySelector('header nav a[href*=' +id + ']').classList.add('active');
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
             });
-        };
+        }
     });
+
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
 };
+
+scrollReveal({
+    reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+
+scrollReveal().reveal('.home-content, .heading', { origin: 'top' });
